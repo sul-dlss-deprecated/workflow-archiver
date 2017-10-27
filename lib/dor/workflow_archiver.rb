9e8771da59d4c16dfbba15dcdf2525b65ec50205
Tony Zanella <tony.zanella@gmail.com>
2017-10-27 09:41:36 -0700

    workflow_archiver: only process first 600 objects (until we fix cron not to fire if job is still running

--- lib/dor/workflow_archiver.rb
+++ lib/dor/workflow_archiver.rb
@@ -262,7 +262,7 @@ module Dor
       end
 
       LyberCore::Log.info "Found #{objs.size.to_s} completed workflows"
-      objs = objs.first(500) # FIXME: temporarily limit objs processed until we fix cron not to fire if job still running
+      objs = objs.first(600) # FIXME: temporarily limit objs processed until we fix cron not to fire if job still running
 
       archiving_criteria = map_result_to_criteria(objs)
       with_indexing_disabled { archive_rows(archiving_criteria) }
